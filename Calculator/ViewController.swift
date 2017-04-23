f3fd296e2b450f19c0c782d0b5bbbcb4a8ff2938
Raymond Pau <raymondpau@raymonds-imac.local>
2017-04-24 06:59:41 +0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Raymond Pau on 24/4/17.
+//  Copyright © 2017 Raymond Pau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
